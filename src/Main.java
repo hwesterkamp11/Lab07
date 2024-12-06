d73be10bbc5b0a5217435545c1b306ae96de4cf7
hunter <westerhr@mail.uc.edu>
2024-12-06 13:35:34 -0500

    task 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        for (int count = 0; count <=30; count++) {
+        for (int count = 30; count >= 0; count--) {
             System.out.println(count);
         }
     }
